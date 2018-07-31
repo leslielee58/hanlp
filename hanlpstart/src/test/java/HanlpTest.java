1f64278837011e03b1d066bdbc837ef4275c8b61
Leslie Lee <leslie@LesliedeMacBook-Pro.local>
2018-07-31 23:23:16 +0800

    nlp segment test

---
+++ hanlpstart/src/test/java/HanlpTest.java
@@ -0,0 +1,18 @@
+import com.hankcs.hanlp.HanLP;
+import com.hankcs.hanlp.seg.common.Term;
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * @author Leslie Lee
+ * CreateOn 2018/7/31
+ */
+public class HanlpTest {
+
+    @Test
+    public void should_nlp() {
+        List<Term> terms = HanLP.segment("今天天气不错哦，一起来玩吧");
+        terms.forEach(term -> System.out.println(term));
+    }
+}
